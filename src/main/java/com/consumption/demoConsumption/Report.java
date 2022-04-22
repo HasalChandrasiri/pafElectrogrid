c98b7c1329896d63ed366d5fa2a10441f9f49a40
Hasal <hasalc@gmail.com>
2022-04-22 12:52:42 +0530

    first commit

---
+++ src/main/java/com/consumption/demoConsumption/Report.java
@@ -0,0 +1,28 @@
+package com.consumption.demoConsumption;
+
+public class Report {
+	private int reportID ;
+	private int customerID ;
+	private String category ;
+	
+	public int getReportID() {
+		return reportID;
+	}
+	public void setReportID(int reportID) {
+		this.reportID = reportID;
+	}
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+
+}
---
+++ src/main/java/com/consumption/demoConsumption/ReportResource.java
@@ -0,0 +1,16 @@
+package com.consumption.demoConsumption;
+
+import jakarta.ws.rs.Path;
+
+@Path("reports")
+public class ReportResource {
+	/*public Report getReport() {
+		
+		Report r1 = new Report() ;
+		r1.setReportID(01);
+		r1.setCustomerID(001);
+		r1.setCategory(null);
+		return r1 ;;
+	}*/
+
+}
