dcadf6d8e340c3c3118deefb367a5d32b7e3fbae
Hashani-Mandira <88082421+Hashani-Mandira@users.noreply.github.com>
2022-04-22 15:46:45 +0530

    initial

---
+++ src/main/java/com/consumption/demoConsumption/PowerCutSchedule.java
@@ -0,0 +1,44 @@
+package com.consumption.demoConsumption;
+
+public class PowerCutSchedule {
+	
+	private int pid;
+	private String area;
+	private String startTime;
+	private String endTime;
+	private String group;
+	
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	
+}
